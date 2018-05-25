42ba5e165f34a2e99288488ebfad280af11dc029
Hemanthx <30410784+Hemanthx@users.noreply.github.com>
2018-05-25 18:24:21 +0530

    Add files via upload

---
+++ notmnist.py
@@ -0,0 +1,320 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+from IPython.display import display
+from PIL import Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+get_ipython().magic(u'matplotlib inline')
+
+
+# In[4]:
+
+
+num_classes = 10
+np.random.seed(133)
+
+def maybe_extract(filename, force=False):
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+  data_folders = [
+    os.path.join(root, d) for d in sorted(os.listdir(root))
+    if os.path.isdir(os.path.join(root, d))]
+  if len(data_folders) != num_classes:
+    raise Exception(
+      'Expected %d folders, one per class. Found %d instead.' % (
+        num_classes, len(data_folders)))
+  print(data_folders)
+  return data_folders
+
+
+# In[5]:
+
+
+os.chdir('Downloads')
+
+
+# In[6]:
+
+
+os.getcwd()
+
+
+# In[7]:
+
+
+train_folders =maybe_extract('notMNIST_large.tar.gz')
+test_folders = maybe_extract('notMNIST_small.tar.gz')
+
+
+# In[10]:
+
+
+from IPython.display import display, Image
+
+
+# In[14]:
+
+
+display(Image(filename='notMNIST_small/A/MDEtMDEtMDAudHRm.png'))
+
+
+# In[15]:
+
+
+display(Image(filename='notMNIST_small/B/Q2FsYW1pdHkgVGVlbiBCVE4gQm9sZC50dGY=.png'))
+
+
+# In[16]:
+
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+def load_letter(folder, min_num_images):
+  """Load the data for a single letter label."""
+  image_files = os.listdir(folder)
+  dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
+                         dtype=np.float32)
+  image_index = 0
+  print(folder)
+  for image in os.listdir(folder):
+    image_file = os.path.join(folder, image)
+    try:
+      image_data = (ndimage.imread(image_file).astype(float) - 
+                    pixel_depth / 2) / pixel_depth
+      if image_data.shape != (image_size, image_size):
+        raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+      dataset[image_index, :, :] = image_data
+      image_index += 1
+    except IOError as e:
+      print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
+    
+  num_images = image_index
+  dataset = dataset[0:num_images, :, :]
+  if num_images < min_num_images:
+    raise Exception('Many fewer images than expected: %d < %d' %
+                    (num_images, min_num_images))
+    
+  print('Full dataset tensor:', dataset.shape)
+  print('Mean:', np.mean(dataset))
+  print('Standard deviation:', np.std(dataset))
+  return dataset
+        
+def maybe_pickle(data_folders, min_num_images_per_class, force=False):
+  dataset_names = []
+  for folder in data_folders:
+    set_filename = folder + '.pickle'
+    dataset_names.append(set_filename)
+    if os.path.exists(set_filename) and not force:
+      # You may override by setting force=True.
+      print('%s already present - Skipping pickling.' % set_filename)
+    else:
+      print('Pickling %s.' % set_filename)
+      dataset = load_letter(folder, min_num_images_per_class)
+      try:
+        with open(set_filename, 'wb') as f:
+          pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+      except Exception as e:
+        print('Unable to save data to', set_filename, ':', e)
+  
+  return dataset_names
+
+train_datasets = maybe_pickle(train_folders, 45000)
+test_datasets = maybe_pickle(test_folders, 1800)
+
+
+# In[20]:
+
+
+for x in range(0,10):
+    pickle_file = train_datasets[x]
+    with open(pickle_file , 'rb') as f:
+        letter_set = pickle.load(f)
+        sampleind = np.random.randint(len(letter_set))
+        sample_img = letter_set[sampleind, :, :]
+        plt.figure()
+        plt.imshow(sample_img)
+
+
+# In[27]:
+
+
+for x in range(0,10):
+    pickle_file = train_datasets[x]
+    with open(pickle_file , 'rb') as f:
+        set1 = pickle.load(f)
+        print('lenght' , len(set1)) #printing is different in ipython lol
+
+
+# In[28]:
+
+
+def make_arrays(nb_rows, img_size):
+  if nb_rows:
+    dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
+    labels = np.ndarray(nb_rows, dtype=np.int32)
+  else:
+    dataset, labels = None, None
+  return dataset, labels
+
+def merge_datasets(pickle_files, train_size, valid_size=0):
+  num_classes = len(pickle_files)
+  valid_dataset, valid_labels = make_arrays(valid_size, image_size)
+  train_dataset, train_labels = make_arrays(train_size, image_size)
+  vsize_per_class = valid_size // num_classes
+  tsize_per_class = train_size // num_classes
+    
+  start_v, start_t = 0, 0
+  end_v, end_t = vsize_per_class, tsize_per_class
+  end_l = vsize_per_class+tsize_per_class
+  for label, pickle_file in enumerate(pickle_files):       
+    try:
+      with open(pickle_file, 'rb') as f:
+        letter_set = pickle.load(f)
+        # let's shuffle the letters to have random validation and training set
+        np.random.shuffle(letter_set)
+        if valid_dataset is not None:
+          valid_letter = letter_set[:vsize_per_class, :, :]
+          valid_dataset[start_v:end_v, :, :] = valid_letter
+          valid_labels[start_v:end_v] = label
+          start_v += vsize_per_class
+          end_v += vsize_per_class
+                    
+        train_letter = letter_set[vsize_per_class:end_l, :, :]
+        train_dataset[start_t:end_t, :, :] = train_letter
+        train_labels[start_t:end_t] = label
+        start_t += tsize_per_class
+        end_t += tsize_per_class
+    except Exception as e:
+      print('Unable to process data from', pickle_file, ':', e)
+      raise
+    
+  return valid_dataset, valid_labels, train_dataset, train_labels
+            
+            
+train_size = 200000
+valid_size = 10000
+test_size = 10000
+
+valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(
+  train_datasets, train_size, valid_size)
+_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
+
+print('Training:', train_dataset.shape, train_labels.shape)
+print('Validation:', valid_dataset.shape, valid_labels.shape)
+print('Testing:', test_dataset.shape, test_labels.shape)
+
+
+# In[29]:
+
+
+def randomize(dataset, labels):
+  permutation = np.random.permutation(labels.shape[0])
+  shuffled_dataset = dataset[permutation,:,:]
+  shuffled_labels = labels[permutation]
+  return shuffled_dataset, shuffled_labels
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+
+
+# In[31]:
+
+
+pickle_file = os.path.join(os.getcwd(), 'notMNIST.pickle')
+
+try:
+  f = open(pickle_file, 'wb')
+  save = {
+    'train_dataset': train_dataset,
+    'train_labels': train_labels,
+    'valid_dataset': valid_dataset,
+    'valid_labels': valid_labels,
+    'test_dataset': test_dataset,
+    'test_labels': test_labels,
+    }
+  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+  f.close()
+except Exception as e:
+  print('Unable to save data to', pickle_file, ':', e)
+  raise
+
+
+# In[32]:
+
+
+statinfo = os.stat(pickle_file)
+print('Compressed pickle size:', statinfo.st_size)
+
+
+# In[55]:
+
+
+regression = LogisticRegression(solver='sag')
+
+
+# In[56]:
+
+
+X_test = test_dataset.reshape(test_dataset.shape[0], 28 * 28)
+y_test = test_labels
+
+
+# In[57]:
+
+
+sample_size = 5000
+X_train = train_dataset[:sample_size].reshape(sample_size, 784)
+y_train = train_labels[:sample_size]
+
+
+# In[59]:
+
+
+regression.fit(X_train,y_train)
+
+
+# In[60]:
+
+
+regression.score(X_test,y_test)
+
+
+# In[61]:
+
+
+regression.predict(X_test)
+
+
+# In[62]:
+
+
+from sklearn.metrics import accuracy_score 
+accuracy = accuracy_score(y_test,regression.predict(X_test))
+
+
+# In[63]:
+
+
+accuracy
+
